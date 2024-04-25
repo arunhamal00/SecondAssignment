5d0d5c7bfd600ca8119d44a7f6b818b33ba2fda8
Arun Hamal <thakuriarun565@gmail.com>
2024-04-25 18:32:43 +0545

    second assignment file

---
+++ secondproject/Person.cs
@@ -0,0 +1,12 @@
+public class Person
+ {
+    public int index;
+    public string userId;
+    public string firstName;
+    public string lastName;
+    public string Gender;
+    public string email;
+    public string phone;
+    public DateTime dob;
+    public string jobTitle;
+}
---
+++ secondproject/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CsvParser parser = new();
+        List<Person> people = parser.Parse();
+        // parser.PrintNames();
+        PeopleReport.SaveMales(people);
+        PeopleReport.SaveFemales(people);
+        PeopleReport.SaveDotComUsers(people);
+    }
+}
